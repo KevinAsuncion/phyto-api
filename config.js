8b2996356a9efde1b0d53cfe0c23cc3b3700caa4
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-06-15 14:08:59 -0700

    added config file

---
+++ config.js
@@ -0,0 +1,10 @@
+"use strict";
+
+exports.DATABASE_URL =
+    process.env.DATABASE_URL || "mongodb://localhost/phyto";
+exports.TEST_DATABASE_URL =
+    process.env.TEST_DATABASE_URL || "mongodb://localhost/test-phyto";
+exports.PORT = process.env.PORT || 8080;
+exports.CLIENT_ORIGIN = "http://localhost:3000"
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || "7d";
