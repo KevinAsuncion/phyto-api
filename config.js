529ab26ebce112be2ce8e121d01a523be189802e
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-06-16 15:00:36 -0700

    update config file

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ exports.TEST_DATABASE_URL =
     process.env.TEST_DATABASE_URL || "mongodb://localhost/test-phyto";
 exports.PORT = process.env.PORT || 8080;
 exports.CLIENT_ORIGIN = "http://localhost:3000" 
-exports.JWT_SECRET = process.env.JWT_SECRET || 'phyto-app'
+exports.JWT_SECRET = process.env.JWT_SECRET
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
