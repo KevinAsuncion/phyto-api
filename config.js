7ef76261c3c8516199ca7797fc253ad8458bbd9a
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-06-25 19:35:42 -0700

    update config

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ exports.DATABASE_URL =
 exports.TEST_DATABASE_URL =
     process.env.TEST_DATABASE_URL || "mongodb://localhost/test-phyto";
 exports.PORT = process.env.PORT || 8080;
-// exports.CLIENT_ORIGIN = "https://optimistic-swartz-84bc04.netlify.com"
-exports.CLIENT_ORIGIN = "http://localhost:3000" || "https://optimistic-swartz-84bc04.netlify.com"   
-exports.JWT_SECRET = process.env.JWT_SECRET || 'phyto'
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+exports.CLIENT_ORIGIN = "https://optimistic-swartz-84bc04.netlify.com"
+// exports.CLIENT_ORIGIN = "http://localhost:3000"   
+exports.JWT_SECRET = process.env.JWT_SECRET 
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY 
